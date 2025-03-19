c12b5d9d8fd082cffab081c6e2ce538ae629a54a
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 19:10:52 +0800

    Ensures that the input is valid

--- print_6_digit_number_0_on_left.py
+++ print_6_digit_number_0_on_left.py
@@ -1,4 +1,13 @@
-# Ask the user to input a number from 0 - 1000
-number = int(input("Please input a number from 0 to 1000 only: "))
 # Ensure that the user input is a number and within the limits
+while True:
+    try:
+        # Ask the user to input a number from 0 - 1000
+        number = int(input("Please input a number from 0 to 1000 only: "))
+        if 0 <= number <= 1000:
+            print(number)
+            break
+        else:
+            print("Invalid input! Please enter a number between 0 and 1000.")
+    except ValueError:
+        print("Invalid input. Please only input numbers")
 # Print the result with trailing zeros to complete 6 digits
