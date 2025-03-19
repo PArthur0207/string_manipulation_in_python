b11af5168b1d27054ca38a1ac8cfa8cd93a90bd1
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 19:13:34 +0800

    Print the result with trailing zeros to complete 6 digits

--- print_6_digit_number_0_on_left.py
+++ print_6_digit_number_0_on_left.py
@@ -4,10 +4,11 @@ while True:
         # Ask the user to input a number from 0 - 1000
         number = int(input("Please input a number from 0 to 1000 only: "))
         if 0 <= number <= 1000:
-            print(number)
             break
         else:
             print("Invalid input! Please enter a number between 0 and 1000.")
     except ValueError:
         print("Invalid input. Please only input numbers")
+
 # Print the result with trailing zeros to complete 6 digits
+print(f'{number:06}')
