be2830780a4ae5d45a7df0731027f88837833628
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 20:58:28 +0800

    Print the result with PascalCase

--- input_full_name_with_incorrect_casing_print_pascal_case.py
+++ input_full_name_with_incorrect_casing_print_pascal_case.py
@@ -1,3 +1,4 @@
 # Ask the user to input their full name with incorrect casing
 fullname = input("Please input your full name with incorrect casing: ")
 # Print the result using PascalCase
+print(fullname.title().replace(" ", ""))
