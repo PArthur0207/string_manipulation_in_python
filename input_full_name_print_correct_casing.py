9a26c4c52b6ba5e9f31a2f35b79582f24aad83ca
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 19:31:45 +0800

    Print the input with correct casings

--- input_full_name_print_correct_casing.py
+++ input_full_name_print_correct_casing.py
@@ -1,3 +1,4 @@
 # Asks the user to input their full name
 fullname = input("Please input your full name: ")
 # Print the result with the first letter only is capitalized in each word
+print(fullname.title())
