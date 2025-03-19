6ebaabb4c6550428e39a2e43f0328b418a955008
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 15:05:46 +0800

    Asks the user to input their full name

--- input_full_name_with_spaces_in_left_print_without_spaces_in_left.py
+++ input_full_name_with_spaces_in_left_print_without_spaces_in_left.py
@@ -1,2 +1,3 @@
 # Ask the user to input full name with spaces in the beggining
+fullname = input("Please inpute your full name with spacees in the beginning: ")
 # Print the result without the spaces in the beginning
