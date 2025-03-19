c46430f4e8ca70213544d9c23255def9556429ef
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 15:07:36 +0800

    Prints the result with the spaces in the beginning removed

--- input_full_name_with_spaces_in_left_print_without_spaces_in_left.py
+++ input_full_name_with_spaces_in_left_print_without_spaces_in_left.py
@@ -1,3 +1,4 @@
 # Ask the user to input full name with spaces in the beggining
 fullname = input("Please inpute your full name with spacees in the beginning: ")
 # Print the result without the spaces in the beginning
+print(fullname.lstrip())
