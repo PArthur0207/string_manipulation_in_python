922bfa5d9e74dc371657a6920adeea841184f755
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-19 19:06:56 +0800

    Ask the user to input a number

--- print_6_digit_number_0_on_left.py
+++ print_6_digit_number_0_on_left.py
@@ -1,3 +1,4 @@
 # Ask the user to input a number from 0 - 1000
+number = int(input("Please input a number from 0 to 1000 only: "))
 # Ensure that the user input is a number and within the limits
 # Print the result with trailing zeros to complete 6 digits
